87a4fc976e34540748ac05d6a18cde2e04be5c8d
Krishnaswamy Subramanian <jskswamy@gmail.com>
2019-05-26 17:01:10 +0530

    Remove unnecessary timeout logic when gracefully stopping the server

--- cmd/server.go
+++ cmd/server.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"github.com/boothgames/nightfury/api"
 	"github.com/boothgames/nightfury/cmd/cli"
@@ -14,7 +13,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 // serverCmd represents the server command
@@ -22,53 +20,14 @@ var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Start nightfury server and broadcast its information using mdns",
 	Run: func(cmd *cobra.Command, args []string) {
-		router := gin.Default()
-		err := db.Initialize(dbPath)
-		cli.DieIf(err)
-
-		cli.DieIf(err)
-		cleanup := func() {
-			clients := nightfury.Clients{}
-			repository := db.DefaultRepository()
-			clientsFromRepo, err := nightfury.NewClientsFromRepo(repository)
-			cli.DieIf(err)
-
-			mapstructure.Decode(clientsFromRepo, &clients)
-			cli.Warn("deleting all clients from db")
-			clients.Delete(repository)
-
-			cli.Warn("shutting down db")
-			err = db.Close()
-			cli.DieIf(err)
-		}
-
-		api.Bind(router)
-		srv := &http.Server{
-			Addr:    fmt.Sprintf("%s:%d", bindAddress, bindPort),
-			Handler: router,
-		}
-
-		go func() {
-			// service connections
-			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-				cli.DieIf(err)
-			}
-		}()
+		go startServer()
 
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 		<-quit
-		cli.Warn("\ngracefully shutting down server ...")
 
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		if err := srv.Shutdown(ctx); err != nil {
-			cli.Success("server shutdown:", err)
-		}
-		select {
-		case <-ctx.Done():
-			cleanup()
-		}
+		cli.Warn("\ngracefully shutting down server ...")
+		cleanup()
 		cli.Success("done")
 	},
 }
@@ -82,4 +41,41 @@ func init() {
 	serverCmd.Flags().StringVarP(&bindAddress, "bind-address", "", "0.0.0.0", "specify the advertise address to use")
 	serverCmd.Flags().IntVarP(&bindPort, "bind-port", "p", 5624, "specify the advertise port to use")
 	serverCmd.Flags().StringVarP(&dbPath, "db-path", "", "nightfury.db", "specify the database path where db will be stored")
+}
+
+func startServer() {
+	router := gin.Default()
+	err := db.Initialize(dbPath)
+	cli.DieIf(err)
+
+	api.Bind(router)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", bindAddress, bindPort),
+		Handler: router,
+	}
+
+	// service connections
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		cli.DieIf(err)
+	}
+}
+
+func cleanup() {
+	clients := nightfury.Clients{}
+	repository := db.DefaultRepository()
+	clientsFromRepo, err := nightfury.NewClientsFromRepo(repository)
+	cli.DieIf(err)
+
+	err = mapstructure.Decode(clientsFromRepo, &clients)
+	cli.DieIf(err)
+
+	cli.Warn("deleting all clients from db")
+
+	err = clients.Delete(repository)
+	cli.DieIf(err)
+
+	cli.Warn("shutting down db")
+	err = db.Close()
+	cli.DieIf(err)
 }
